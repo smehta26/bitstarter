377b844f10462cae7986bbce27fed8bba86aea24
Saurabh Mehta <saurabh.mehta.26@gmail.com>
2013-07-11 04:03:21 +0000

    Updating web.js for reading

--- web.js
+++ web.js
@@ -2,8 +2,9 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var buffer = new Buffer(fs.readFileSync("index.html"), "utf-8");
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(buffer);
 });
 
 var port = process.env.PORT || 5000;
